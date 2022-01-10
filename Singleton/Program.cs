4fb590fa5b2b2b6b19f1ff0533a9c6e609061d79
Goonnya <95735892+Goonnya@users.noreply.github.com>
2022-01-10 11:33:00 +0400

    Добавьте файлы проекта.

---
+++ Singleton/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Manager
+    {
+        static Manager manager;
+
+        protected Manager()
+        {
+            Console.WriteLine("Добро пожаловать, новый менеджер!");
+        }
+        // Возвращается единственный объект менеджера
+        public static Manager Instance()
+        {
+            if (manager == null)
+                manager = new Manager();
+            else
+                Console.WriteLine("Два менеджера не может существовать одновременно");
+            return manager;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Manager u = new Manager();
+
+            Manager manager1 = Manager.Instance();
+
+            Manager manager2 = Manager.Instance();
+            // Повторим создание. Instance вернет уже созданный объект.
+
+            Console.Read();
+        }
+    }
+}
